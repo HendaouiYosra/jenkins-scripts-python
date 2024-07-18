3eef189f66c6e133a94fb4b8550c1b3e52464e5d
HendaouiYosra <106975299+HendaouiYosra@users.noreply.github.com>
2024-07-18 12:36:14 +0200

    Update install_jenkins.py

--- install_jenkins.py
+++ install_jenkins.py
@@ -3,7 +3,7 @@ import subprocess
 def install_jenkins_module():
     try:
         # Use pip to install jenkins package
-        subprocess.check_call(['pip', 'install', 'jenkins'])
+        subprocess.check_call(['pip', 'install', 'python-jenkins'])
         print("jenkins module installed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Failed to install jenkins module. Error: {e}")
